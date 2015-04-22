3695a64de71847fd5330f9e2c76c1b718db50629
Ben Cornelis & Jennifer Minetree <student@epicodus.com>
2015-04-22 13:31:41 -0700

    write second spec and pass.

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -2,6 +2,8 @@ class String
   define_method(:beats?) do |opponent|
     if self.eql?("rock") && opponent.eql?("scissors")
       true
+    elsif self.eql?("rock") && opponent.eql?("paper")
+      false  
     end
   end
 end
